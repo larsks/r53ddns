833742cf7789898951dd2d3d4dfa910c42a204c5
Lars Kellogg-Stedman <lars@redhat.com>
2015-04-13 10:31:43 -0400

    pep8 fixes

--- r53ddns/model.py
+++ r53ddns/model.py
@@ -1,16 +1,16 @@
 # r53ddns -- dynamic dns server for Route53
 # Copyright (C) 2015 Lars Kellogg-Stedman
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -52,7 +52,7 @@ class Credentials(db.Entity):
     secretkey = Required(str)
     hosts = Set("Host")
     created = Required(datetime,
-                           default=datetime.utcnow)
+                       default=datetime.utcnow)
 
     composite_key(owner, accesskey, secretkey)
     composite_key(owner, name)
